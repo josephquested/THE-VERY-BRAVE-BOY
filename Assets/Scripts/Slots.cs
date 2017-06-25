07bad5bcac928aeccb6e0f2eeb6ef0af9aaacf63
josephquested <josephquested@gmail.com>
2017-06-25 19:25:27 +1200

    add slot icon

--- Assets/Scripts/Slots.cs
+++ Assets/Scripts/Slots.cs
@@ -6,11 +6,15 @@ public class Slots : MonoBehaviour {
 
 	// EQUIPMENT //
 
+	public SpriteRenderer[] slotIconRenderer = new SpriteRenderer[4];
+	public Animator[] slotAnims = new Animator[4];
 	public Equipment[] equipmentInSlot = new Equipment[4];
 
 	public void ReceiveEquipment (Equipment equipment)
 	{
 		equipment.EquipToParent(transform);
 		equipmentInSlot[(int)equipment.slotType] = equipment;
+		slotAnims[(int)equipment.slotType].SetTrigger("Activate");
+		slotIconRenderer[(int)equipment.slotType].sprite = equipment.gameObject.GetComponent<SpriteRenderer>().sprite;
 	}
 }
